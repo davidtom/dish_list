8ef2bcc90ec0b08410f2c4c2e5a29f5cc8c876e0
n3m3sis42 <n3m3sis42@gmail.com>
2017-06-30 13:15:17 -0400

    fixed association issue

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,5 @@
 class Restaurant < ActiveRecord::Base
   belongs_to :type
   has_many :restaurant_users
-  has_many :users, through: :restaurant_user
+  has_many :users, through: :restaurant_users
 end
